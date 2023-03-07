4188d51457cafe08533452398bbd71855627a9a0
Rudra <patelrudra.3000@gmail.com>
2023-03-08 01:13:18 +0530

    Added Assets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
+import Image from "next/image";
+import Under from "../assets/under.svg";
+import Cone from "../assets/cone.svg";
+
 export default function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-black text-white">
+      <div className="flex justify-center items-center md:space-x-32">
+      <div className="hidden md:flex justify-center items-center">
+        <Image src={Under} alt="Under Construction" width={200} height={200} />
+      </div>
+      <div className="flex-col space-y-16 md:space-y-0">
+      <div className="flex md:hidden justify-center items-center">
+        <Image src={Under} alt="Under Construction" width={100} height={100} />
+      </div>
       <div className="flex-col space-y-10">
-        <h1 className="text-5xl">Rudra's Portfolio</h1>
-        <h2 className="flex justify-center items-center text-2xl">Under Construction 🛠</h2>
+        <h1 className="flex justify-center items-center text-5xl truncate">Rudra's Portfolio</h1>
+        <h2 className="flex justify-center items-center text-2xl truncate">Under Construction 🛠</h2>
+      </div>
+      <div className="flex md:hidden justify-center items-center">
+        <Image src={Cone} alt="Under Construction" width={100} height={100} />
+      </div>
+      </div>
+      <div className="hidden md:flex justify-center items-center">
+        <Image src={Cone} alt="Under Construction" width={200} height={200} />
+      </div>
       </div>
     </div>
   )
